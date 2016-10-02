ed5a08bc414e118f05f16ae29b3287250603ae06
cycbot <parkcyc666@gmail.com>
2016-10-02 17:12:08 +0800

    u

---
+++ Sort Colors/Sort Colors.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int right=nums.size()-1;
+        int left=0;
+        for(int i=0;i<=right;i++){
+            while(nums[i]==2&&i<right)swap(nums[i],nums[right--]);
+            while(nums[i]==0&&i>left)swap(nums[i],nums[left++]);
+        }
+    }
+};
