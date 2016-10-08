478fab3dff9b6a7f2276a927a7e215fccffe46ac
cycbot <parkcyc666@gmail.com>
2016-10-08 20:27:17 +0800

    u

---
+++ Ugly Number 2/Ugly Number 2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        if(n==1)return 1;
+        vector<int> ans(n);
+        int t2=0,t3=0,t5=0;
+        ans[0]=1;
+        for(int i=1;i<n;i++){
+            ans[i]=min(ans[t2]*2,min(ans[t3]*3,ans[t5]*5));
+            if(ans[i]==ans[t2]*2)t2++;
+            if(ans[i]==ans[t3]*3)t3++;
+            if(ans[i]==ans[t5]*5)t5++;
+        }
+        return ans[n-1];
+    }
+};
